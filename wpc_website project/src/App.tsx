eaa1962e0aea1a0a0bd0a5b43acd0939fbfbc33c
wanshm <wanshuim@nycstudents.net>
2025-04-07 20:01:42 +0000

    created an interface for the data for mcq component

--- wpc_website project/src/App.tsx
+++ wpc_website project/src/App.tsx
@@ -1,10 +1,20 @@
 
 import './App.css'
 
+interface McqDataProps {
+  question : string;
+  answer : number; 
+  choices : string[];
+  imgsrc ? : string;
+}
+
 function App() {
+
+  const mcqData:McqDataProps[] = [{question: "1+1 = ", answer : 0, choices : ["2", "b", "5", "fishy"]}]
 
   return (
     <>
+      
     </>
   )
 }
