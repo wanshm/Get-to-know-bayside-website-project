f731e663a3a979b57980317384859c053f1086ee
colinl11 <colinl@nycstudents.net>
2025-04-28 19:52:12 +0000

    added data of names, gmail, and roles they have.

---
+++ wpc_website project/src/Data.tsx
@@ -0,0 +1,185 @@
+const list = [{
+    "staff": 1,
+    "first_name": "Tracy ",
+    "last_name": "Martinez",
+    "email": "tmartinez3@schools.nyc.gov",
+    "position": "Administration",
+    "role": "Principal"
+  }, {
+    "staff": 2,	
+    "first_name": "Jonathan ",
+    "last_name": "Hirata",
+    "email": "kassel@schools.nyc.gov",
+    "position": "Administration",
+    "role": "Assistant Principal Organization"
+  }, {
+    "staff": 3,
+    "first_name": "Karen ",
+    "last_name": "Assel",
+    "email": "tbollanos@schools.nyc.gov",
+    "role": "Art, Music & Languages"
+  }, {
+    "staff": 4,
+    "first_name": "George ",
+    "last_name": "Brianka",
+    "email": "gbrianka@schools.nyc.gov",
+    "role": "Art, Music & Languages"
+  }, {
+    "staff": 5,
+    "first_name": "Anastasia ",
+    "last_name": "Caamano",
+    "email": "acaamano@schools.nyc.gov",
+    "role": "Art, Music & Languages"
+  }, {
+    "staff": 6,
+    "first_name": "Jessica",
+    "last_name": "Camas",
+    "email": "jcamas@schools.nyc.gov",
+    "role": "Art, Music & Languages"
+  }, {
+    "staff": 7,
+    "first_name": "Antony ",
+    "last_name": "Ambrose",
+    "email": "aambros3@schools.nyc.gov",
+    "role": "English"
+  }, {
+    "staff": 8,
+    "first_name": "Jeremy",
+    "last_name": "Ball",
+    "email": "jball4@schools.nyc.gov",
+    "role": "English"
+  }, {
+    "staff": 9,
+    "first_name": "Marc",
+    "last_name": "Belinfanti",
+    "email": "mbelinfanti@schools.nyc.gov",
+    "role": "English"
+  }, {
+    "staff": 10,
+    "first_name": "Khana",
+    "last_name": "Aminov",
+    "email": "KKatayeva2@schools.nyc.gov",
+    "role": "Mathematics"
+  }, {
+    "staff": 11,
+    "first_name": "Melissa",
+    "last_name": "Amster",
+    "email": "mamster2@schools.nyc.gov",
+    "role": "Mathematics"
+  }, {
+    "staff": 12,
+    "first_name": "Michael ",
+    "last_name": "Barbosa",
+    "email": "mbarbosa2@schools.nyc.gov",
+    "role": "Mathematics"
+  }, {
+    "staff": 13,
+    "first_name": "Emilio ",
+    "last_name": "Alverio",
+    "email": "ealverio2@schools.nyc.gov",
+    "role": "Science"
+  }, {
+    "staff": 14,
+    "first_name": "Jeffrey ",
+    "last_name": "Pollack",
+    "email": "jpollack3@schools.nyc.gov",
+    "role": "Science"
+  }, {
+    "staff": 15,
+    "first_name": "Jason ",
+    "last_name": "Ban",
+    "email": "jban2@schools.nyc.gov",
+    "role": "Science"
+  }, {
+    "staff": 16,
+    "first_name": "Erica ",
+    "last_name": "Amato",
+    "email": "eamato6@schools.nyc.gov",
+    "role": "Social Studies"
+  }, {
+    "staff": 17,
+    "first_name": "Stephanie ",
+    "last_name": "Fox",
+    "email": "sfox14@schools.nyc.gov",
+    "role": "Social Studies"
+  }, {
+    "staff": 18,
+    "first_name": "Terence ",
+    "last_name": "McNamara",
+    "email": "tmcnamara@schools.nyc.gov",
+    "role": "Social Studies"
+  }, {
+    "staff": 19,
+    "first_name": "Natalie ",
+    "last_name": "Berrouet",
+    "email": "nberrouet@schools.nyc.gov",
+    "role": "Guidance Department"
+  }, {
+    "staff": 20,
+    "first_name": "Jennifer ",
+    "last_name": "Chu-Choy",
+    "email": "jchuchoy@schools.nyc.gov",
+    "role": "Guidance Department"
+  }, {
+    "staff": 21,
+    "first_name": "Sanderson",
+    "last_name": "Bensley",
+    "email": "XSI",
+    "role": "Guidance Department"
+  }, {
+    "staff": 22,
+    "first_name": "Alexander ",
+    "last_name": "Mandalar",
+    "email": "amandalar@schools.nyc.gov",
+    "position": "Computer Technology Specialist",
+    "role": "Office Staff"
+  }, {
+    "staff": 23,
+    "first_name": "Cecilia ",
+    "last_name": "Cross",
+    "email": "Secretary",
+    "position": "ccross5@schools.nyc.gov",
+    "role": "Office Staff"
+  }, {
+    "staff": 24,
+    "first_name": "Carllie ",
+    "last_name": "Jaxen",
+    "email": "cjaxen@schools.nyc.gov",
+    "position": "SPARK / Youth Development",
+    "role": "Office Staff"
+  }, {
+    "staff": 25,
+    "first_name": "Greg ",
+    "last_name": "Bonsignore",
+    "email": "gbonsig@schools.nyc.gov",
+    "position": "Attendance Clerk",
+    "role": "Office Staff"
+  }, {
+    "staff": 26,
+    "first_name": "Susan ",
+    "last_name": "Seidita",
+    "email": "sseidita@schools.nyc.gov",
+    "position": "Secretary",
+    "role": "Office Staff"
+  }]
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
