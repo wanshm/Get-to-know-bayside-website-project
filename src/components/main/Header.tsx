356379669dc9a0d608ae669b918bf50707e12fe2
MaxG53 <MAxg53@nycstudents.net>
2025-04-30 05:14:39 +0000

    checking previous commit

--- src/components/main/Header.tsx
+++ src/components/main/Header.tsx
@@ -1,33 +1,44 @@
 import './header.css';
+import { Link } from 'react-router';
 
 interface HeaderProps {
-  setCurrentPage: (page: string) => void;
-  searchTerm: string;
-  setSearchTerm: (term: string) => void;
-  handleSearch: (e: React.FormEvent) => void;
+  setCurrentPage?: (page: string) => void;
+  searchTerm?: string;
+  setSearchTerm?: (term: string) => void;
+  handleSearch?: (e: React.FormEvent) => void;
 }
 
-export default function Header({ setCurrentPage, searchTerm, setSearchTerm, handleSearch }: HeaderProps) {
+export default function Header({
+  setCurrentPage,
+  searchTerm,
+  setSearchTerm,
+  handleSearch,
+}: HeaderProps) {
   return (
-    <header className="header">
-      <div className="header-title">
-        <h1><span className="highlight">Get To Know </span>Bayside!</h1>
+    <header className='header'>
+      <div className='header-title'>
+        <h1>
+          <span className='highlight'>Get To Know </span>Bayside!
+        </h1>
       </div>
-      
-      <nav className="nav-links">
-        <a onClick={() => setCurrentPage('Home')}>Home</a>
-        <a onClick={() => setCurrentPage('multiple')}>Multiple Choice</a>
-        <a onClick={() => setCurrentPage('naming')}>Naming</a>
+
+      <nav className='nav-links'>
+        <h1>
+          <span className='highlight'>Quiz </span>Types:{' '}
+        </h1>
+        <Link to={'/'}>Home</Link>
+        <Link to={'/Mcq'}>Multiple Choice</Link>
+        <Link to={'/Naming'}>Naming</Link>
       </nav>
-      
-      <form onSubmit={handleSearch} className="search-container">
+
+      <form onSubmit={handleSearch} className='search-container'>
         <input
-          type="text"
-          placeholder="Search quiz type..."
+          type='text'
+          placeholder='Search quiz type...'
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <button type="submit">Search</button>
+        <button type='submit'>Search</button>
       </form>
     </header>
   );
