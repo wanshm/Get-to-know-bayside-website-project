a28fee8963a0d6bce0db6aff8e1ba7657b22e89a
MaxG53 <MAxg53@nycstudents.net>
2025-04-30 03:58:50 +0000

    Revision of Header, Footer, and App.tsx and App.CSS which are to be revised in a further commit.

--- src/components/main/footer.tsx
+++ src/components/main/footer.tsx
@@ -1,9 +1,10 @@
-import './Footer.css';
+import './footer.css';
 
 
 export default function Footer(){
     return(
         <footer className="footer">
+            <img src="/main/bhslogo.png"  className="footer-logo" />
             <p> Max Goldstein, Wanshui Ma, Colin Liang</p>
         </footer>
     );
